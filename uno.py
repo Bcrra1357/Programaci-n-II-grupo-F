3e02c927663f7ee36a94ba8c427b8c2789ff15af
Esteban <esteban6n10bcrra@gmail.com>
2025-08-25 08:54:50 -0400

    Tarea

---
+++ uno.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+#Ventana
+ventanaPrincipal = tk.Tk()
+ventanaPrincipal.title("Sistema de Registro de Pacientes")
+ventanaPrincipal.geometry("600x450")
+ventanaPrincipal.configure(bg="#BBBBBB")
+
+ventanaPrincipal.mainloop()
